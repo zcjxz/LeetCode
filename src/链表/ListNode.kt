38695381ef204bf1ef7d69f8f3563b5b18b97187
zcj <1067986407@qq.com>
2021-08-17 21:27:33 +0800

    提交【链表】添加 创建链表 和 打印链表的基础方法

--- src/链表/ListNode.kt
+++ src/链表/ListNode.kt
@@ -3,3 +3,33 @@ package 链表
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
+
+/**
+ * 传入一个数组，创建一个链表
+ */
+fun createListNode(array: IntArray): ListNode {
+    val head = ListNode(array[0])
+    var current = head
+    for (i in 1 until array.size){
+        val next = ListNode(array[i])
+        current.next = next
+        current = next
+    }
+    return head
+}
+
+/**
+ * 打印链表
+ */
+fun printListNode(listNode: ListNode?){
+    if (listNode==null){
+        print("null")
+        return
+    }
+    print("${listNode.`val`}、")
+    var next = listNode.next
+    while (next!=null){
+        print("${next.`val`}、")
+        next = next.next
+    }
+}
