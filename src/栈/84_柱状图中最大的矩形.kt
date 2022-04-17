569fe572e1f71b30e09d3148c8e3120d96e30c3e
zcj <1067986407@qq.com>
2022-04-17 23:26:17 +0800

    二刷 84

--- src/栈/84_柱状图中最大的矩形.kt
+++ src/栈/84_柱状图中最大的矩形.kt
@@ -6,12 +6,13 @@ fun largestRectangleArea(heights: IntArray): Int {
     // 由于 Kotlin 中入参 无法改变，只能 copy 多一份
     val copy = IntArray(heights.size+2)
     heights.copyInto(copy,1)
-    // 放置哨兵
-    copy[0] = 0
-    copy[copy.size-1] = 0
+    // 放置哨兵，其实下面两行不用写也可以， int 默认填充是 0
+//    copy[0] = 0
+//    copy[copy.size-1] = 0
     // 提前放置哨兵，在 while 循环中可以不用判空
     stack.push(0)
     var max = 0
+    // 这里可以从 1 开始
     for (i in copy.indices){
         // 注意这里要循环弹出来
         while (copy[stack.peek()]>copy[i]){
@@ -24,6 +25,26 @@ fun largestRectangleArea(heights: IntArray): Int {
     return max
 }
 
+/**
+ * 第二次做
+ */
+fun largestRectangleArea_T2(heights: IntArray): Int {
+    val stack = java.util.ArrayDeque<Int>()
+    val newHeight = IntArray(heights.size+2)
+    heights.copyInto(newHeight,1)
+    stack.push(0)
+    var max = 0
+    for (i in 1 until newHeight.size){
+        while (newHeight[i]<newHeight[stack.peek()]){
+            val height = newHeight[stack.pop()]
+            val width = i - stack.peek() -1
+            max = Math.max(height*width,max)
+        }
+        stack.push(i)
+    }
+    return max
+}
+
 fun main() {
-    print(largestRectangleArea(intArrayOf(2, 1,2)))
+    print(largestRectangleArea_T2(intArrayOf(2, 1,2)))
 }
